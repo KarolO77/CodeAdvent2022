53865ebfec0a285dd7d7a59239ea99e773083f56
KarolO77 <110632116+KarolO77@users.noreply.github.com>
2023-10-07 21:41:30 +0200

    Add files via upload

---
+++ Day#6.py
@@ -0,0 +1,15 @@
+with open("day6.txt") as file:
+    data = [i for i in file.read().strip().splitlines()]
+    data = "".join(data)
+    data = [i for i in data]
+for i in range(4, len(data)):
+    storage = set(data[(i - 4) : i])
+    if len(storage) == 4:
+        print("Answer to part 1:", i)
+        break
+
+for i in range(14, len(data)):
+    storage = set(data[(i - 14) : i])
+    if len(storage) == 14:
+        print("Answer to part 2:", i)
+        break
